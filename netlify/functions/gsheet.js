63b26f2710c53a183a97c3d68142aaf1692690f4
Nour431998 <n.marei@alahly.com>
2026-01-19 14:27:44 +0200

    Update gsheet.js

--- netlify/functions/gsheet.js
+++ netlify/functions/gsheet.js
@@ -1,1 +1,32 @@
+exports.handler = async (event) => {
+  const GAS_BASE =
+    "https://script.google.com/macros/s/AKfycbwMtciGVBDO8N7bcT03wJFW4Jb8NDVlW2omC9coa9t6Im8zSri5haEzWjmJaBulela8/exec";
+
+  const qs = event.rawQuery || "";
+  const target = qs ? `${GAS_BASE}?${qs}` : GAS_BASE;
+
+  try {
+    const r = await fetch(target);
+    const text = await r.text();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "Access-Control-Allow-Origin": "*",
+        "Cache-Control": "public, max-age=300"
+      },
+      body: text
+    };
+  } catch (err) {
+    return {
+      statusCode: 502,
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({ error: "Proxy fetch failed", details: String(err) })
+    };
+  }
+};
 
